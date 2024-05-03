b3e59b7d3a1324192931cdadeeebe7851c2f9b5d
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2024-05-03 13:49:43 -0500

    improving gui appearance and functionality

--- qt_gui.py
+++ qt_gui.py
@@ -1,12 +1,29 @@
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtCore, QtWidgets, QtGui
 import qt_simulate as simulate
+import sys
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("REVIVE Calculator Tool")
+        # set up app identity
+        self.app_name = "REVIVE Calculator Tool"
+        self.settings = QtCore.QSettings("Phius", self.app_name)
+        self.icon = QtGui.QIcon()
+        self.icon.addFile("Phius-Logo-RGB__Color_Icon.ico")
 
+        # customize window
+        self.setWindowTitle(self.app_name)
+        self.setWindowIcon(self.icon)
+
+        # create top-level widgets
+        self.file_entry_layout = QtWidgets.QGridLayout()
+        self.file_entry_groupbox = QtWidgets.QGroupBox("File Entry")
+        self.run_options_layout = QtWidgets.QHBoxLayout()
+        self.run_options_groupbox = QtWidgets.QGroupBox("Run Options")
+        self.sim_button = QtWidgets.QPushButton("Simulate")
+
+        # hard-code widget labels TODO: change this somehow?
         self.widget_labels = ["Batch Name",
                               "IDD File Name",
                               "Study/Output Folder",
@@ -17,21 +34,14 @@ class MyWidget(QtWidgets.QWidget):
                               "Generate Graphs?",
                               "Delete Unnecessary Files?"]
 
-        # create top-level widgets
-        self.file_entry_layout = QtWidgets.QVBoxLayout()
-        self.file_entry_groupbox = QtWidgets.QGroupBox("File Entry")
-        self.run_options_layout = QtWidgets.QHBoxLayout()
-        self.run_options_groupbox = QtWidgets.QGroupBox("Run Options")
-        self.sim_button = QtWidgets.QPushButton("Simulate")
-
         # create all inner-level widgets
         self.batch_name = QtWidgets.QLineEdit()
         self.file_entry_widgets = {}
         for field in self.widget_labels[1:5]:
-               self.file_entry_widgets[field] = QtWidgets.QLineEdit()
-        self.gen_pdf_option = QtWidgets.QCheckBox()
-        self.gen_graphs_option = QtWidgets.QCheckBox()
-        self.del_files_option = QtWidgets.QCheckBox()
+            self.file_entry_widgets[field] = QtWidgets.QLineEdit()
+        self.gen_pdf_option = QtWidgets.QCheckBox(self.widget_labels[6])
+        self.gen_graphs_option = QtWidgets.QCheckBox(self.widget_labels[7])
+        self.del_files_option = QtWidgets.QCheckBox(self.widget_labels[8])
         self.num_parallel_procs = QtWidgets.QComboBox()
         self.progress_bar = QtWidgets.QProgressBar()
 
@@ -43,26 +53,48 @@ class MyWidget(QtWidgets.QWidget):
         
         # populate the top-level widgets with child widgets
         self.create_file_entry_widgets()
+        self.create_run_options_widgets()
         
         # enable the big simulate button
         self.sim_button.clicked.connect(self.simulate)
 
+        # create a message box for error popups
+        self.error_msg_box = QtWidgets.QMessageBox()
+        self.error_msg_box.setIcon(QtWidgets.QMessageBox.Critical)
+        self.error_msg_box.setWindowTitle("Error")
+        self.error_msg_box.setWindowIcon(self.icon)
+
+        # create a message box for completion alert
+        self.completion_msg_box = QtWidgets.QMessageBox()
+        self.completion_msg_box.setIcon(QtWidgets.QMessageBox.Information)
+        self.completion_msg_box.setWindowTitle(self.app_name)
+        self.completion_msg_box.setWindowIcon(self.icon)
+
 
     def create_file_entry_widgets(self):
         # preface file entry area with batch name widget
-        self.file_entry_layout.addWidget(self.batch_name)
+        batch_label = QtWidgets.QLabel()
+        batch_label.setText(self.widget_labels[0])
+        self.file_entry_layout.addWidget(batch_label, 0, 0)
+        self.file_entry_layout.addWidget(self.batch_name, 0, 1)
 
         # add file explorer widgets to layout
         self._open_file_explorer_actions = {}
-        for widget_key, qline in self.file_entry_widgets.items():
+        for i, (widget_key, qline) in enumerate(self.file_entry_widgets.items()):
 
             # create the actions
             self._open_file_explorer_actions[widget_key] = qline.addAction(
                 qApp.style().standardIcon(QtWidgets.QStyle.SP_DirOpenIcon),  # noqa: F821
                 QtWidgets.QLineEdit.TrailingPosition)
             
+            # populate fields with last used information, blank string as default
+            qline.setText(self.settings.value(widget_key, ""))
+            
             # add to layout
-            self.file_entry_layout.addWidget(qline)
+            qlabel = QtWidgets.QLabel()
+            qlabel.setText(widget_key)
+            self.file_entry_layout.addWidget(qlabel, i+1, 0)
+            self.file_entry_layout.addWidget(qline, i+1, 1)
 
         # connect the actions
         self._open_file_explorer_actions[self.widget_labels[1]].triggered.connect( # IDD file
@@ -76,6 +108,35 @@ class MyWidget(QtWidgets.QWidget):
             
         # apply layout to groupbox widget
         self.file_entry_groupbox.setLayout(self.file_entry_layout)
+
+    
+    def create_run_options_widgets(self):
+        # label and add option items for parallel processes
+        qlabel = QtWidgets.QLabel()
+        qlabel.setText(self.widget_labels[5]) # Parallel processes
+        self.num_parallel_procs.addItems([str(x) for x in [1,2,4,8,12,16,20,24,28,32]])
+
+        # build the left half with parallel process selection
+        left_half = QtWidgets.QVBoxLayout(alignment=QtCore.Qt.AlignHCenter)
+        left_half.addStretch()
+        left_half.addWidget(qlabel)
+        left_half.addWidget(self.num_parallel_procs)
+        left_half.addStretch()
+
+        # build the right half with with checkbox options
+        right_half = QtWidgets.QVBoxLayout(alignment=QtCore.Qt.AlignHCenter)
+        right_half.addStretch()
+        right_half.addWidget(self.gen_pdf_option, QtCore.Qt.AlignHCenter)
+        right_half.addWidget(self.gen_graphs_option, QtCore.Qt.AlignHCenter)
+        right_half.addWidget(self.del_files_option, QtCore.Qt.AlignHCenter)
+        right_half.addStretch()
+
+        # incorporate both left and right halves
+        self.run_options_layout.addLayout(left_half)
+        self.run_options_layout.addLayout(right_half)
+
+        # apply layout to groupbox widget
+        self.run_options_groupbox.setLayout(self.run_options_layout)
 
 
     @QtCore.Slot()
