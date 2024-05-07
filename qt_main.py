bb9798083ccb4e156b2c5d821c88b49aa73764c1
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2024-05-07 00:33:00 -0500

    adding page for multiphase adorb calculator

--- qt_main.py
+++ qt_main.py
@@ -6,7 +6,7 @@ from PySide6 import QtWidgets
 if __name__=="__main__":
     app = QtWidgets.QApplication([])
 
-    widget = gui.MyWidget()
+    widget = gui.MyWidget("-t" in sys.argv or "--test" in sys.argv)
     widget.resize(800, 600)
     widget.show()
 
