42234ae329a8e92f82a59073941819997ead4a9f
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2024-05-09 15:59:17 -0500

    connecting gui to multiphase adorb

--- REVIVE2024/gui.py
+++ REVIVE2024/gui.py
@@ -24,6 +24,7 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtGui import QIcon, QIconEngine
 import simulate
+import adorb
 
 class MyWidget(QWidget):
 
@@ -273,13 +274,13 @@ class MPAdorbTab(QWidget):
         self.layout = QVBoxLayout()
         self.phases_layout = QVBoxLayout()
         self.add_phase_button = QPushButton("Add additional phase")
-        self.complete_button = QPushButton("Complete")
+        self.compute_button = QPushButton("Compute Multiphase ADORB")
 
         # add widgets to main layout
         self.layout.addLayout(self.phases_layout)
         self.layout.addWidget(self.add_phase_button)
         self.layout.addStretch()
-        self.layout.addWidget(self.complete_button)
+        self.layout.addWidget(self.compute_button)
         
         # init adorb constants
         self.phase_count = 1
@@ -287,8 +288,8 @@ class MPAdorbTab(QWidget):
         self.num_sim_years = 70
 
         # store the results of the phases
-        self.file_entries = {}
-        self.year_entries = {}
+        self.file_entries = []
+        self.year_entries = []
 
         # keep track of current phase widgets
         self.phase_widgets = []
@@ -299,6 +300,7 @@ class MPAdorbTab(QWidget):
 
         # enable the buttons
         self.add_phase_button.clicked.connect(self.spawn_phase_entry_widget)
+        self.compute_button.clicked.connect(self.compute_mp_adorb)
 
         # set the layout
         self.setLayout(self.layout)
@@ -314,22 +316,15 @@ class MPAdorbTab(QWidget):
         # create box for labels, file entry, and year entry
         phase_layout = QHBoxLayout()
 
-        # create file entry box and connect action
+        # create file entry boxes
         file_entry_box = QLineEdit()
-        curr_count = self.phase_count
-        self.file_entries[curr_count] = file_entry_box
-        action = file_entry_box.addAction(
-            qApp.style().standardIcon(QStyle.SP_DirOpenIcon),  # noqa: F821
-            QLineEdit.TrailingPosition)
-        action.triggered.connect(
-            lambda _ : self.on_open_file(curr_count)
-        )
+        self.file_entries.append(file_entry_box)
 
         # create year dropdown box
         year_start_box = QComboBox()
         year_start_box.addItems([str(x) for x in range(self.num_sim_years)])
         year_start_box.resize(200,100)
-        self.year_entries[self.phase_count] = year_start_box
+        self.year_entries.append(year_start_box)
 
         # create labels
         file_entry_label = QLabel("ADORB Results File:")
@@ -359,6 +354,15 @@ class MPAdorbTab(QWidget):
         # connect delete button
         del_button.clicked.connect(lambda _ : self.delete_phase_entry_widget(new_phase_widget))
 
+        # connect browse files button
+        action = file_entry_box.addAction(
+            qApp.style().standardIcon(QStyle.SP_DirOpenIcon),  # noqa: F821
+            QLineEdit.TrailingPosition)
+        action.triggered.connect(
+            lambda _ : self.on_open_file(new_phase_widget)
+        )
+
+
     @Slot()
     def delete_phase_entry_widget(self, widget):
         # remove the widget from the window
@@ -370,13 +374,16 @@ class MPAdorbTab(QWidget):
         for i in range(len(self.phase_widgets)-1, idx, -1):
             self.phase_widgets[i].setTitle(self.phase_widgets[i-1].title())
         
-        # remove from list
+        # remove widget and entries from list
         self.phase_widgets.pop(idx)
+        self.file_entries.pop(idx)
+        self.year_entries.pop(idx)
         self.phase_count -= 1
 
 
     @Slot()
-    def on_open_file(self, phase_id):
+    def on_open_file(self, groupbox: QGroupBox):
+        phase_id = self.phase_widgets.index(groupbox)
         qline = self.file_entries[phase_id]
         prompt = f"Select File: Phase {phase_id}"
         
@@ -385,3 +392,22 @@ class MPAdorbTab(QWidget):
 
         dest = QDir(path)
         qline.setText(QDir.fromNativeSeparators(dest.path()))
+
+
+    @Slot()
+    def compute_mp_adorb(self):
+        # collect arguments to send to simulate function
+        adorb_paths = [x.text() for x in self.file_entries]
+        year_starts = [x.currentText() for x in self.year_entries]
+        
+        # input validation
+        err_string = adorb.validate_input(adorb_paths, year_starts)
+
+        # run the computation
+        try:
+            assert err_string == "", err_string
+            adorb.multiphaseADORB(adorb_paths, year_starts)
+        except Exception as err_msg:
+            self.parent.display_error(str(err_msg))
+        else:
+            self.parent.display_info("Computation complete!")
