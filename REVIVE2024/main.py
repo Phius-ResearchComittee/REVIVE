07845370442423f83d32f242e7169b260378a9ad
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2024-06-29 01:55:35 -0500

    adjusting main app sizing/style

--- REVIVE2024/main.py
+++ REVIVE2024/main.py
@@ -8,9 +8,10 @@ if __name__=="__main__":
     mp.freeze_support()
     mp.set_start_method("spawn")
     app = QApplication([])
+    app.setStyle("fusion")
 
     widget = gui.MyWidget("-t" in sys.argv or "--test" in sys.argv)
-    widget.resize(800, 600)
+    widget.resize(1000, 600)
     widget.show()
 
     sys.exit(app.exec())
